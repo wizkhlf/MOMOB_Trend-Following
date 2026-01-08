1256eb2f215802dfdeea93c130ff90168530dcf6
wizkhlf <wzacrypto@gmail.com>
2026-01-08 19:33:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from app import app
-
+import os
 if __name__ == "__main__":
     url = "http://127.0.0.1:5002"
 
@@ -7,5 +7,9 @@ if __name__ == "__main__":
     print(f" Cliquer pour ouvrir l'application : {url}")
     print("============================================\n")
 
-    # On lance le serveur Flask
-    app.run(debug=True, host="0.0.0.0", port=5002)
+    
+
+    port = int(os.environ.get("PORT", 5002))
+    app.run(host="0.0.0.0", port=port)
+
+
